9b5ead8ef3e6098eaf8c1793654ef3ca5512ac94
park0691 <mem29238@gmail.com>
2020-07-06 00:46:09 +0900

    initialized project

---
+++ ClassPlus/src/main/java/pnu/classplus/ClassPlusApplication.java
@@ -0,0 +1,13 @@
+package pnu.classplus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassPlusApplication.class, args);
+	}
+
+}
